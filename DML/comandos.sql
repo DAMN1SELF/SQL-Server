5519af1affb209215e55796c7f741e01d868ad54
DAMN1SELF <85372343+DAMN1SELF@users.noreply.github.com>
2025-03-23 20:38:29 -0500

    variables

--- DML/comandos.sql
+++ DML/comandos.sql
@@ -15,8 +15,11 @@ select AddressTypeID,Name,rowguid,ModifiedDate from Person.AddressType;
 --select AddressTypeID,Name,rowguid,ModifiedDate from AddressType;
 
 
-select * from [Production].[Product] where color is NOT NULL AND color != 'Blue' ;
-select COALESCE(Color, 'Sin Color') ,*  from [Production].[Product] where (ReorderPoint > 500 AND  [name] like '%s') OR color = 'Silver';
+select DATEADD(YEAR, 1, SellStartDate) ,* from [Production].[Product] where color is NOT NULL AND color != 'Blue' AND ReorderPoint BETWEEN  650 and 1500;
+select DATEDIFF(DAY, SellStartDate, SellEndDate) ,* from [Production].[Product] where SellEndDate IS NOT NULL AND SellStartDate IS NOT NULL  ;
+select SUBSTRING([ProductNumber],CHARINDEX('-',[ProductNumber])+1,CHARINDEX('-', [ProductNumber], 2)+1),PARSENAME(REPLACE([ProductNumber], '-', '.'), 2) AS Resultado
+,LEN([Name]),* from [Production].[Product] where SellEndDate IS NOT NULL AND SellStartDate IS NOT NULL  ;
+select COALESCE(Color, 'Sin Color'),UPPER([Name]),LOWER([Name]) ,*  from [Production].[Product] where (ReorderPoint > 500 AND  [name] like '%s') OR color = 'Silver';
 
 -- INSERT
 
